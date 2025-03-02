fdd962f420a834ba1896c13983fd9a30139ed877
DelTechs01 <bleamalika@gmail.com>
2025-03-02 13:00:06 +0300

    Displaying the contents from the members table.

--- Church.php
+++ Church.php
@@ -10,4 +10,25 @@ if ($conn-> connect_error) {
     die("Connection Failed" . $conn -> connect_error);
 }
 
+#Display the contents from the members table
+$sql = "SELECT * FROM members";
+$result - $conn-> query($sql);
+if ($result -> num_rows > 0) {
+    echo "<table border='1'>
+        <tr>
+            <th>Name</th>
+            <th>Tithe Number</th>
+            <th>Registration Number</th>
+        </tr>";
+while ($row = $result -> fetch_assoc()) {
+    echo "<tr>
+    <td>" . $row["name"]. "</td>
+    <td>" . $row["tithe_number"] . "</td>
+    <td>" . $row["registration_number"] . "</td>
+    </tr>";
+}
+echo "</table>";
+} else {
+    echo "No Members Found";
+}
 ?>
